aad0ef8dd4d80d1c62835363e84fd8ba3dca612a
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-07-10 21:20:49 -0300

    Add gem devise_token_auth and configure

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  
   namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
+  
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :products
     end
