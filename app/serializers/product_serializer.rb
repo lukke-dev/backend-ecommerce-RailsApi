0d5cc53c1584158e08c92e3295a231381ab6be2e
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-07-15 22:23:34 -0300

    add config to storage

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
-  include ActionView::Helpers
-  include Rails.application.routes.url_helpers
-
   attributes :id, :name, :description, :price, :quantity
 
   attribute :imageUrl do
