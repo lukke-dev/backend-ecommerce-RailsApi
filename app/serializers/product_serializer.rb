e86c258c43d1f8ecbf9f9ab0131068f80975fdc6
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-07-16 11:40:14 -0300

    Change serializer price product

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,12 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :quantity
+  include ActionView::Helpers::NumberHelper
+  attributes :id, :name, :description, :quantity
 
   attribute :imageUrl do
     @object.image_url.url
+  end
+
+  attribute :price do
+    number_to_currency(@object.price, precision: 2, unit: "R$")
   end
 end
