e05059297dfa2d6cf8d56b089a9299f95611fdbc
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-07-17 18:57:53 -0300

    update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -88,7 +88,17 @@ namespace :deploy do
   after  :finishing,    :cleanup
   # after  :finishing,    :restart
 end
-
+before "deploy:assets:precompile", "deploy:yarn_install"
+namespace :deploy do
+  desc "Run rake yarn install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
